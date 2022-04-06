b89abe9059341ff2801b062243f8f1d7a0015eac
Umut Emre <umutemreonder@gmail.com>
2022-04-06 23:23:53 +0300

    Comment Linelar Silindi Gondermeye Hazir (y)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner scannerInput = new Scanner(System.in);
-        System.out.print("Input File Name: ");
+        System.out.print("Input File Name (e.g. input.txt): ");
         String fileName = scannerInput.nextLine();
         System.out.print("Byte Ordering: ");
         char byteType = scannerInput.nextLine().charAt(0);
@@ -38,10 +38,6 @@ public class Main {
         return numberString.charAt(numberString.length() - 1) == 'u' ? Integer.parseInt(numberString.substring(0, numberString.length() - 1)) : Integer.parseInt(numberString);
     }
 
-    // fixedSize sadece fraction kismindan cagrildiysa fonksiyon false oluyor
-    // int signed kisminin amaci 16 bitlik unsigned ya da signed gonderildiginde eger unsigned ise tum bitleri kullanabilir
-    // ama signed ise ilk biti signe icin ayirmali o yuzden for dongusu sondan 2. bitten (yani sondan 1. indexten) baslatmali
-    // bu dedigim fraction kismindan gelenler icin gecerli olmadigindan fixedSize booleani ekledim
     public static int[] integerToBit(String numberString, int size, boolean fixedSize) {
         int[] answer = new int[size];
         int number = stringToInt(numberString);
@@ -61,7 +57,6 @@ public class Main {
         return answer;
     }
 
-    // ilk 1'i gorene kadar bir sey yapmiyor, gordukten sonra her biti ters ceviriyor
     public static void translateToNegative(int[] answer) {
         boolean firstSeen = false;
         for(int i = 15; i >= 0; i--) {
@@ -70,7 +65,6 @@ public class Main {
         }
     }
 
-    // ustalik eseri fonksiyonum bi yandan assembly dinlerken bi yandan kodladim anlayan comment ekleyebilir
     public static String bitToHex(int[] bits) {
         StringBuilder answer = new StringBuilder();
         for(int k = 1; k <= bits.length / 4; k++) {
@@ -83,9 +77,6 @@ public class Main {
         return answer.toString();
     }
 
-    // endian b ise oldugu gibi birakiyor sadece 2 karakterde bir bosluk ekliyor
-    // hoca efendiler bosluksuz input kabul etmedigi icin boyle sikten bir sey yazmak zorunda kaldim
-    // l ise de ters ceviriyo iste
     public static String endian(String hex, char endian) {
         StringBuilder newHex = new StringBuilder();
 
@@ -105,7 +96,6 @@ public class Main {
         return newHex.toString();
     }
 
-    // allah bu fonksiyonu anlamak isteyene sabir versin
     public static int[] floatToBit(double a, int size) {
         boolean negative = a < 0;
         a = Math.abs(a);
@@ -130,10 +120,7 @@ public class Main {
 
         int[] roundedSum = new int[fractionSize];
         round(sum, fractionSize);
-        // eger sum arrayinin ilk elementi rounddan sonra 0 olduysa
-        // demekki sum arrayi soyle bisiydi --> 1.111|111
-        // roundlaninca 0.000 oldu normalde 10.000 olup sonra 1.0000'a donusmesi gerekiyor da onla ugrasmaya gerek yok onemsiz bi part cunku
-        // eger ilk bit rounddan sonra 0 olduysa demek ki 1 virgil daha kayacak exp + 1 alacak
+
         exp = sum[0] == 0 ? exp + 1: exp;
 
         int[] expArray = integerToBit(String.valueOf(exp), (int) (Math.log(exp) / Math.log(2) + 1), false);
@@ -156,18 +143,10 @@ public class Main {
         return bitLast;
     }
 
-    // fraction kismi icin round fonksiyonu
     public static void round(int[] sum, int fractionSize) {
-        // eger fraction kismi zaten istedigimiz uzunlukta ya da daha kisaysa veya siradaki int 0 ise herhangi bir round yapmadan yolluyoruz
-        // 0 kontrolunun amaci eger siradaki int 0sa zaten roundUp yok oldugu gibi kalacak
-        // 111|01111111 -> 111
         if (sum.length - 1 <= fractionSize || sum[fractionSize + 1] == 0) return;
         boolean repeat = false;
 
-        // ilk once ilk 1den sonra baska bi 1 var mi diye bakiyoruz, neden? cunku eger baska bi 1 varsa direkt roundUp yapicaz
-        // yoook eger yoksa roundEven yapicaz
-        // 110|10000 -> 110
-        // 110|10001 -> 111
         for(int i = fractionSize + 2; i < sum.length; i++) {
             if (sum[i] == 1) {
                 repeat = true;
@@ -175,10 +154,8 @@ public class Main {
             }
         }
 
-        // eger roundEven yapacaksak ve ilk bit 0 ise hicbir sey yapmadan yolluyoz ustte ornegini verdim zaten
         if (!repeat && (sum[fractionSize] == 0)) return;
 
-        // eger degilse de ilk 0'i gorene kadar hepsini ters ceviriyoz 0'i gorunce onu da ters cevirip duruyoz
         for (int i = fractionSize; i >= 0; i--) {
             if (sum[i] == 1) sum[i] = 0;
             else {
@@ -188,16 +165,12 @@ public class Main {
         }
     }
 
-    // Guzel optimize edilmis kral fonksiyon, doublein noktadan sonraki kismini bite cevirir
     public static List<Integer> fractionToBit(double fraction) {
         List<Integer> fractionArray = new ArrayList<>();
-        // Neden bu fonksiyon 25 kere donuyo gardas? Cunku en fazla 24 bite kadar gidebilir flptsize = 4 ise 1 tane fazladan donduruoz
-        // ki roundlayacagimiz yerin ilk bitini kesin olarak bilelim
+
         for (int i = 1; i <= 25; i++) {
-            // eger fraction 0 olduysa zaten kalan elemanlar 0 olacak bi isimize yaramiyo o yuzden durabilir
             if (fraction == 0) break;
 
-            // o zamana kadar 2^-i kadar cikarma yapiyo iste
             if (fraction >= Math.pow(2, -i)) {
                 fractionArray.add(1);
                 fraction -= Math.pow(2, -i);
@@ -206,11 +179,7 @@ public class Main {
                 fractionArray.add(0);
             }
         }
-        // isin tatli kismi burasi
-        // simdi bura nie var? Cunku roundUp mi roundEven mi yapicaz bilmek icin bize fraction kisminin tamami gerek ama
-        // ya 300. bitte 1 tane 1 gelecekse? ya da 3000. bitte? aminakoim sonsuza kadar loop mu dondurecez?
-        // o yuzden 25 bitten sonra fraction kismi hala 0 olmadiysa yani hala eklenecek bitler varsa nerede olduklarinin bi onemi yok zaten
-        // o yuzden 1 tane 1 ekleyip gecebiliriz o kisimlar roundUp olacak o yuzden umrumuzda degil
+
         if (fraction != 0) fractionArray.add(1);
         return fractionArray;
     }
